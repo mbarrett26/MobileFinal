7db3fea93a4a4ba16653520891addf837edf983f
Mithusan <mithusan.arul11@gmail.com>
2023-12-03 20:02:14 -0500

    final commit v3

--- app/src/main/java/com/example/mobilefinalproject/MainActivity.java
+++ app/src/main/java/com/example/mobilefinalproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mobilefinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -13,17 +15,21 @@ import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity{
 
+    private static final String CART_PREFS = "cart_prefs";
+    private static final String CART_ITEMS_KEY = "cart_items";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
+        clearCartInSharedPreferences();
     }
 
+    public void clearCartInSharedPreferences(){
+        SharedPreferences sharedPreferences = this.getSharedPreferences(CART_PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
 
-
+        editor.putString(CART_ITEMS_KEY, "");
+        editor.apply();
+    }
 }
